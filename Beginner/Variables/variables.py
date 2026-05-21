30b95b965e56362f38bc81b67ee5404663d779a0
mounikavelidindi <mounikavelidindi044@gmsil.com>
2026-05-21 13:27:54 +0530

    Completed variables task

---
+++ Beginner/Variables/variables.py
@@ -0,0 +1,20 @@
+# Task 1
+
+# 1
+pi = 22/7
+print("Value of pi:", pi)
+print("Data type:", type(pi))
+
+# 2
+# for = 4
+
+print("Cannot use 'for' as variable because it is a keyword.")
+
+# 3
+P = 10000
+R = 5
+T = 3
+
+SI = (P * R * T) / 100
+
+print("Simple Interest:", SI)
